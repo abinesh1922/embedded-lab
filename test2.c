1db4f62dcac081457af788a4589c84783e306762
abinesh1922 <abivarshini1922@gmail.com>
2025-02-06 21:47:38 +0530

    Third day

--- test2.c
+++ test2.c
@@ -1,23 +1,131 @@
 #include<stdio.h>
 int main(){
     
-    // int a;
-    // printf ("hello: %d");
-    // scanf("%d",&a);
-    // printf("%d",a);
+//     int a;
+//     printf ("hello: %d");
+//     scanf("%d",&a);
+//     printf("%d",a);
 
 //     unsigned char a=-10;
 //     printf("%d",a);
 //     return 0;
-    // long long int a=9345910859;
-    // printf("%lld",a);
-    // return 0;
+//     long long int a=9345910859;
+//     printf("%lld",a);
+//     return 0;
    
-   int a= 5;
-   int b= 10;
-   if(a<b)
-   {
-    printf("yes, a greater then b");
-   }
+//    int a= 5;
+//    int b= 10;
+//    if(a<b)
+//    {
+//     printf("yes, a greater then b");
+//    }
+//     int num;
+//     printf("Enter the number:");
+//     scanf("%d",&num);
+//     if((num%3)==0){
+//         if((num%5)==0){
+        
+//         }
+//     }
+//     if((num%5)==0){
+        
+//     }
+//     else{
+//         printf("");
+//     }
+    
+    // int a,b,result,choices;
+    // printf("Enter the number-1:");
+    // scanf("%d",&a);
+    // printf("Enter the number-2:");
+    // scanf("%d",&b);
+    // printf("enter the choices from 1 to 4:");
+    // scanf("%d",&choices);
+
+    // switch(choices){
+    //     case 1:
+    //         result=a+b;
+    //         printf("Addition: %d", result);
+    //         break;
+    //     case 2:
+    //         result=a-b;
+    //         printf("Subraction: %d", result);
+    //         break;
+    //     case 3:
+    //         result=a/b;
+    //         printf("%d Division:", result);
+    //         break;
+    //     case 4:
+    //         result=a%b;
+    //         printf("Remainder: %d", result);
+    //         break;
+    //     default:
+    //         printf("Enter the valid Number");
+    //         break;
+    // }
+
+    // char a,result,choices;
+    // printf("Enter the letter:");
+    // scanf("%s",&a);
+    // printf("Enter the choices")
+
+    // int n,i,sum;
+    // sum=0;
+    // printf("Enter N:");
+    // scanf("%d",&n);
+    // for(i=1;i<=n;i++)
+    // {
+    //     sum+=i;
+    //     printf("%d ",sum);
+    // }
+
+    // int n,i;
+    // printf("Enter N:");
+    // scanf("%d",&n);
+    // for(i=0;i<=n;i+=2)
+    // {
+    //     printf("%d ",i);
+    // }
+
+    // int n,i=1,sum=0;
+    // printf("Enter the number:");
+    // scanf("%d",&n);
+    // while(i<=n)
+    // {
+    //     sum=sum+i;
+    //     i++;
+    // }
+    // printf("%d",sum);
+    
+    // int n=1;
+    // do
+    // {
+    //     printf("\ncount:%d",n);
+    //     n++;
+    // } while (n <= 10);
+    // {
+    //     return 0;
+    // }
+
+    int a=2;
+    do
+    {
+        printf("\n%d",a);
+        a=a*2;
+    } while (a<10);
+    {
+        return 0;
+    }
+
+    // int num;
+    // printf("Enter three digit Number:");
+    // scanf("%d",&num);
+    // int ones = num%10;
+    // int tens = (num/10)%10;
+    // int hundreds = num/100;
+    // printf("%d ",ones);
+    // printf("%d ",tens);
+    // printf("%d ",hundreds);
     return 0;
-   }
+    
+}
